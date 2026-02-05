1543fe6a975e4d42ee05a5d8c2b3841edb53e875
Igggosha <igggoshastudios@gmail.com>
2026-02-05 07:36:46 +0000

    Initial commit

---
+++ src/main/java/com/igggosha/morereactions/RegisterReactions.java
@@ -0,0 +1,8 @@
+package com.igggosha.morereactions;
+import com.igggosha.morereactions.reactions.AmideCondensation;
+
+public class RegisterReactions {
+    public static final AmideCondensation AMIDE_CONDENSATION = new AmideCondensation();
+
+    public static void register() {}
+}
---
+++ src/main/java/com/igggosha/morereactions/reactions/AmideCondensation.java
@@ -0,0 +1,54 @@
+package com.igggosha.morereactions.reactions;
+
+import com.igggosha.morereactions.ExampleMod;
+import com.petrolpark.destroy.Destroy;
+import com.petrolpark.destroy.chemistry.legacy.*;
+import com.petrolpark.destroy.chemistry.legacy.genericreaction.DoubleGroupGenericReaction;
+import com.petrolpark.destroy.chemistry.legacy.genericreaction.GenericReactant;
+import com.petrolpark.destroy.chemistry.legacy.index.DestroyGroupTypes;
+import com.petrolpark.destroy.chemistry.legacy.index.DestroyMolecules;
+import com.petrolpark.destroy.chemistry.legacy.index.group.CarboxylicAcidGroup;
+import com.petrolpark.destroy.chemistry.legacy.index.group.PrimaryAmineGroup;
+
+public class AmideCondensation extends DoubleGroupGenericReaction<CarboxylicAcidGroup, PrimaryAmineGroup>
+{
+    public AmideCondensation()
+    {
+        super(ExampleMod.asResource("amide_condensation"), DestroyGroupTypes.CARBOXYLIC_ACID, DestroyGroupTypes.PRIMARY_AMINE);
+    }
+
+    @Override
+    public boolean isPossibleIn(ReadOnlyMixture mixture)
+    {
+        return mixture.getTemperature() >= 100;
+    }
+
+    @Override
+    public LegacyReaction generateReaction(GenericReactant<CarboxylicAcidGroup> first, GenericReactant<PrimaryAmineGroup> second)
+    {
+        LegacyMolecularStructure amineStructure = second.getMolecule().shallowCopyStructure();
+        PrimaryAmineGroup amineGroup = second.getGroup();
+
+        amineStructure.moveTo(amineGroup.nitrogen)
+            .remove(amineGroup.firstHydrogen);
+
+        LegacyMolecularStructure acidStructure = first.getMolecule().shallowCopyStructure();
+        CarboxylicAcidGroup acidGroup = first.getGroup();
+
+        acidStructure.moveTo(acidGroup.carbon)
+            .remove(acidGroup.proton)
+//        acidStructure.moveTo(acidGroup.carbon)
+            .remove(acidGroup.alcoholOxygen);
+
+        LegacySpecies amide = moleculeBuilder().structure(LegacyMolecularStructure.joinFormulae(
+                acidStructure, amineStructure, LegacyBond.BondType.SINGLE
+        )).build();
+
+        return reactionBuilder()
+                .addReactant(first.getMolecule(), 1, 1)
+                .addReactant(second.getMolecule(), 1, 1)
+                .addProduct(amide)
+                .addProduct(DestroyMolecules.WATER)
+                .build();
+    }
+}
